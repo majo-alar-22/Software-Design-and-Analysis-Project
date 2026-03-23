6338096e052f8bdb4dc11055991ca77282b89a6d
Maria Jose Alarcon Osuna <mariajose.alarconosuna@ontariotechu.net>
2026-03-22 20:11:40 -0400

    added UpcomingGamesView tab

--- src/main/java/com/csci2020/frontend/views/LoggedInPanel.java
+++ src/main/java/com/csci2020/frontend/views/LoggedInPanel.java
@@ -3,6 +3,7 @@ package com.csci2020.frontend.views;
 import com.csci2020.backend.Database;
 import com.csci2020.frontend.GameSchedulingView;
 import com.csci2020.frontend.TeamStandingsView;
+import com.csci2020.frontend.UpcomingGamesView;
 
 import javax.swing.*;
 import java.awt.*;
@@ -10,6 +11,8 @@ import java.awt.*;
 public class LoggedInPanel extends JPanel {
     private final JTabbedPane tabbedPane = new JTabbedPane();
     private final Database db;
+    private UpcomingGamesView upcomingGamesView;
+
     public LoggedInPanel(Database db){
         this.db = db;
         this.setLayout(new BorderLayout());
@@ -22,7 +25,16 @@ public class LoggedInPanel extends JPanel {
 
         // this.tabbedPane.addTab("Matches", new JLabel("Not implemented yet"));
         JSpinner spinner = new JSpinner(new SpinnerDateModel());
-        this.tabbedPane.addTab("Matches", new GameSchedulingView(null,null,db,spinner));
+        this.tabbedPane.addTab("Schedule Match", new GameSchedulingView(null,null,db,spinner));
+
+        this.upcomingGamesView = new UpcomingGamesView(db);
+        this.tabbedPane.addTab("Upcoming Matches", upcomingGamesView);
+
+        this.tabbedPane.addChangeListener(e -> {
+            if (this.tabbedPane.getSelectedComponent() == upcomingGamesView) {
+                upcomingGamesView.refreshGames();
+            }
+        });
 
         this.add(tabbedPane);
     }
